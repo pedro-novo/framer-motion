b854c358f6b734d5caa2fcf4aca7d436a050b3df
Pedro Teixeira <pedro.novo.93@gmail.com>
2022-02-03 14:56:28 +0000

    first time exp framer-motion

---
+++ src/components/Order.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    transition: {
+      ease: "easeInOut",
+    },
+  },
+};
+
+const childrenVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
+const Order = ({ pizza, showModalSet }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      showModalSet(true);
+    }, 5000);
+  }, [showModalSet]);
+
+  return (
+    <motion.div
+      className='container order'
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
+      exit='exit'
+    >
+      <h2 exit={{ y: -1000 }}>Thank you for your order :)</h2>
+      <motion.p variants={childrenVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childrenVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Order;
